9e68156ba8cb9bbe86d801eeff059276005d5c63
Florian Geckeler <43751896+fgeck@users.noreply.github.com>
2025-09-01 23:13:20 +0200

    Fix issues in postgres backup (#20)

--- backup-postgres.sh
+++ backup-postgres.sh
@@ -47,7 +47,7 @@ echo "Postgres successfully backupped to file: $(du -h "$BACKUP_FILE" | cut -f1)
 
 export PBS_FINGERPRINT=$PROXMOX_BACKUP_SERVER_FINGERPRINT
 export PBS_PASSWORD=$PROXMOX_BACKUP_SERVER_PASSWORD 
-proxmox-backup-client backup "$BACKUP_NAME:$BACKUP_DIRECTORY" --repository "$PROXMOX_BACKUP_SERVER_REPOSITORY" --backup-id $BACKUP_NAME --ns $PROXMOX_BACKUP_SERVER_NAMESPACE
+proxmox-backup-client backup "$BACKUP_NAME.pxar:$BACKUP_DIRECTORY" --repository "$PROXMOX_BACKUP_SERVER_REPOSITORY" --backup-id $BACKUP_NAME --ns $PROXMOX_BACKUP_SERVER_NAMESPACE
 if [[ $? -ne 0 ]]; then
     ERROR_MSG="$(date '+%Y-%m-%d %H:%M:%S') - Backup failed for $BACKUP_FILE"
     send_telegram_message "$ERROR_MSG"
